039af31706fb9780927a88aa542a442f103cc586
sidharthify <wednisegit@gmail.com>
2025-04-05 05:29:30 +0530

    Bump kernel version to 6.1
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- factory_ravenclaw.mk
+++ factory_ravenclaw.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-TARGET_LINUX_KERNEL_VERSION := 5.10
+TARGET_LINUX_KERNEL_VERSION := 6.1
 
 $(call inherit-product, device/google/gs201/factory_common.mk)
 $(call inherit-product, device/google/pantah/device-ravenclaw.mk)
