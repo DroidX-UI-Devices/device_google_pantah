ae1834225c1b910fd93b417bdebe8e0aca132b66
cyberknight777 <cyberknight755@gmail.com>
2025-04-13 22:44:19 +0530

    pantah: Build settings overlay and add YAAP maintainer name
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -361,7 +361,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SettingsOverlayGFE4J \
     SettingsOverlayGE2AE \
-    SettingsOverlayGP4BC
+    SettingsOverlayGP4BC \
+    SettingsOverlayCheetah
 
 # eng specific
 ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
--- device-panther.mk
+++ device-panther.mk
@@ -346,7 +346,8 @@ PRODUCT_PACKAGES += \
     SettingsOverlayG03Z5 \
     SettingsOverlayGQML3 \
     SettingsOverlayGVU6C \
-    SettingsOverlayGVU6C_VN
+    SettingsOverlayGVU6C_VN \
+    SettingsOverlayPanther
 
 # eng specific
 ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
